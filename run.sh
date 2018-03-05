e5a1c704c532bd05a5abcbbaeb7ed6d598418e7b
Yecheng <1204633887@qq.com>
2018-03-04 22:58:51 -0800

    fix run.sh
    Change-Id: I1cf436191311e430fe3eedda50f07fcd27f068c7

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-for j in blackscholes #canneal #facesim #bodytrack dedup 
+for j in blackscholes #canneal #facesim #bodytrack dedup
 do
-    for i in 2 
+    for i in 2
     do
 	echo "**************running ${j} ${i} ..."
 	echo "    "
@@ -9,11 +9,9 @@ do
 			     --kernel=/home/ryan/gem5_ALPHA_FS/gem5/full_system_images_ALPHA/binaries/vmlinux_2.6.27-gcc_4.3.4 \
 			     --disk-image=/home/ryan/gem5_ALPHA_FS/gem5/full_system_images_ALPHA/disks/linux-parsec-2-1-m5-with-test-inputs.img
 
-	cp m5out/stats.txt ../results/${j}_stats_l1d_${i}.txt
+	cp m5out/stats.txt ../results/${j}_${i}_result.txt
+    cp m5out/config.ini ../results/${j}_${i}_config.ini
 	echo "****************finish ${j} ${i} "
 	echo " "
     done
-done    
-
-
-
+done
